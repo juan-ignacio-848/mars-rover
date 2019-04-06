5c1302066a1089efe08ca6de89fed19384f6dad9
nmkip <juan.ignacio.lopez@redb.ee>
2019-04-06 15:18:02 -0300

    Initial state

---
+++ src/main/java/MarsRoverApplication.java
@@ -0,0 +1,6 @@
+public class MarsRoverApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/Nothing.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class Nothing {
+
+    @Test
+    void nothing() {
+    }
+}
